143eff84d1ebca13ff1a0ee1e4e5cad3af0f0728
astou <gueyeastou682@gmail.com>
2019-12-09 13:21:00 +0100

    Callu de la somme des mots de sentiments

--- BigData/tweet_sentiment.py
+++ BigData/tweet_sentiment.py
@@ -3,5 +3,10 @@ f = open("AFINN-111.txt", "r")
 for line in f:
     if chaine in line:
         print line
+somme  = 0
+chaine = 3
+for i in range(0, chaine):
+    somme = somme + i
+    print somme
 
 f.close()
